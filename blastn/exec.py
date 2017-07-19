82244bceff221b1fdec307140a3dba90f34dfb98
Mitsuki <mitsuo.com.mu@gmail.com>
2017-07-19 18:12:07 +0900

    updated exec.py to cal only strains which does not have the family

---
+++ blastn/exec.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import subprocess
+
+def main(clusterFilepath, strainFilepath):
+    cluster_df=pd.read_csv(clusterFilepath, delimiter='\t')
+    strain_lst=[s.strip() for s in open(strainFilepath, 'r').readlines()]
+
+    for _,row in cluster_df.iterrows():
+        msk=list(row[strain_lst].isnull())# True if the strain does not have the family
+        for i in range(len(strain_lst)):
+            if msk[i]:
+                cmd="/home/mitsuki/altorf/mbgd/blastn/sge_blastn_args.sh {} {}".format(strain_lst[i],row["family"])
+                print(cmd)
+                subprocess.check_call(cmd.strip().split(' '))
+            
+
+if __name__=="__main__":
+    clusterFilepath="/home/mitsuki/altorf/mbgd/data/sampled_cluster.tab"
+    strainFilepath="/home/mitsuki/altorf/mbgd/data/strain.lst"
+    main(clusterFilepath, strainFilepath)
+            
