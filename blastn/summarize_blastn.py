090167bc093577cfda08e24f1cd830049b2c69fe
Mitsuki <mitsuo.com.mu@gmail.com>
2017-07-25 11:43:23 +0900

    summarize by strain

--- blastn/summarize_blastn.py
+++ blastn/summarize_blastn.py
@@ -11,7 +11,7 @@ def output_count(cluster_df, strain_lst, countFilepath):
 		msk=list(row[strain_lst].isnull())# True if the strain does not have the family
 		for i, strain in enumerate(strain_lst):
 			if msk[i]:
-				filepath="./result/{0}_{1}.tab".format(strain,row["family"])
+				filepath="./arxiv/result/{0}_{1}.tab".format(strain,row["family"])
 				dct[strain]=len(open(filepath, 'r').readlines())
 			else:
 				dct[strain]=-1 
@@ -19,33 +19,38 @@ def output_count(cluster_df, strain_lst, countFilepath):
 	count_df=pd.DataFrame(dct_lst)
 	count_df=count_df[["family"]+strain_lst+["lineage", "num_query"]]
 	count_df.to_csv(countFilepath,index=False)
+	print("DONE counting to {}".format(countFilepath))
 
-def output_all(cluster_df, strain_lst, allFilepath):
+def output_strain(strain_lst, family_lst):
 	columns_lst=["qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", 
 							 "qstart", "qend", "sstart","send", "evalue", "bitscore"]
-	
-	dct_lst=[]
-	for _,row in cluster_df.iterrows():
-		msk=list(row[strain_lst].isnull())# True if the strain does not have the family
-		for i, strain in enumerate(strain_lst):
-			if msk[i]:
-				filepath="./result/{0}_{1}.tab".format(strain,row["family"])
+
+	for strain in strain_lst:
+		strainFilepath="./out/{}.csv".format(strain)
+		dct_lst=[]
+
+		for family in family_lst:
+			filepath="./arxiv/result/{0}_{1}.tab".format(strain,family)
+			try:
 				if len(open(filepath, 'r').readlines())>0:
 					df=pd.read_csv(filepath,delimiter='\t', header=None)
-					for _,subrow in df.iterrows():
+					for _,row in df.iterrows():
 						dct={}
-						dct["qfamily"]=row["family"]
-						dct["qstrain"]=subrow[0].split(':')[0]
-						dct["sstrain"]=subrow[1].split(':')[0]
+						dct["qfamily"]=family
+						dct["qstrain"]=row[0].split(':')[0]
+						dct["sstrain"]=row[1].split(':')[0]
 
 						for i,column in enumerate(columns_lst):
-							dct[column]=subrow[i]
+							dct[column]=row[i]
 						dct_lst.append(dct)
-	
-	all_df=pd.DataFrame(dct_lst)
-	all_df=all_df[["qfamily", "qstrain", "sstrain"]+columns_lst]
-	all_df.index.name="region_id"
-	all_df.to_csv(allFilepath)
+			except FileNotFoundError:
+				pass
+		
+		strain_df=pd.DataFrame(dct_lst)
+		strain_df=strain_df[["qfamily", "qstrain", "sstrain"]+columns_lst]
+		strain_df.index.name="region_id"
+		strain_df.to_csv(strainFilepath)
+		print("DONE integration to {}".format(strainFilepath))
 	
 
 if __name__=="__main__":
@@ -57,6 +62,6 @@ if __name__=="__main__":
 
 	countFilepath="./out/count.csv"
 	output_count(cluster_df, strain_lst, countFilepath)
-	
-	allFilepath="./out/all.csv"
-	output_all(cluster_df, strain_lst, allFilepath)
+
+	family_lst=list(cluster_df["family"])
+	output_strain(strain_lst,family_lst)
