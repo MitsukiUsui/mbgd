d46ebfd88e1ec4d8e33c5c712cad3de96cc628a7
Mitsuki <mitsuo.com.mu@gmail.com>
2017-07-25 14:56:22 +0900

    size selection on exec.py

--- blastn/exec.py
+++ blastn/exec.py
@@ -1,21 +1,24 @@
 import pandas as pd
+import numpy as np
 import subprocess
 
 def main(clusterFilepath, strainFilepath):
-    cluster_df=pd.read_csv(clusterFilepath, delimiter='\t')
-    strain_lst=[s.strip() for s in open(strainFilepath, 'r').readlines()]
+	cluster_df=pd.read_csv(clusterFilepath, delimiter='\t')
+	strain_lst=[s.strip() for s in open(strainFilepath, 'r').readlines()]
 
-    for _,row in cluster_df.iterrows():
-        msk=list(row[strain_lst].isnull())# True if the strain does not have the family
-        for i, strain in enumerate(strain_lst):
-            if msk[i]:
-                cmd="/home/mitsuki/altorf/mbgd/blastn/sge_blastn_args.sh {} {}".format(strain,row["family"])
-                print(cmd)
-                subprocess.check_call(cmd.strip().split(' '))
-            
+	for _,row in cluster_df.iterrows():
+		msk=list(row[strain_lst].isnull())# True if the strain does not have the family
+		size=len(strain_lst)-np.sum(msk)
+		if 0<size and size<len(strain_lst):
+			for i, strain in enumerate(strain_lst):
+				if msk[i]:
+					cmd="/home/mitsuki/altorf/mbgd/blastn/sge_blastn_args.sh {} {}".format(strain,row["family"])
+					print(cmd)
+					subprocess.check_call(cmd.strip().split(' '))
+			
 
 if __name__=="__main__":
-    clusterFilepath="/home/mitsuki/altorf/mbgd/data/sampled_cluster.tab"
-    strainFilepath="/home/mitsuki/altorf/mbgd/data/strain.lst"
-    main(clusterFilepath, strainFilepath)
-            
+	clusterFilepath="/home/mitsuki/altorf/mbgd/data/sampled_cluster.tab"
+	strainFilepath="/home/mitsuki/altorf/mbgd/data/strain.lst"
+	main(clusterFilepath, strainFilepath)
+			
